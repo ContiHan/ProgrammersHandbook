d76dfdef625791db560d8ffee91878ff53a4488c
Daniel Hanák <daniel.hanak@outlook.cz>
2023-08-13 18:42:40 +0200

    refactor to class

---
+++ MaximumSubarraySum/Calculation.cs
@@ -0,0 +1,99 @@
+﻿namespace MaximumSubarraySum;
+
+using static Console;
+using static Math;
+
+public class Calculation
+{
+    // var numbers = new[] { -1, 2, 4, -3, 5, 2, -5, 2 };
+    private readonly int[] _numbers;
+
+    public Calculation(byte exponent)
+    {
+        _numbers = Enumerable
+            .Range(0, (int)Pow(10, exponent))
+            .Select(_ => new Random().Next(-10, 11))
+            .ToArray();
+    }
+
+    public void Calculate()
+    {
+        var bestSum = 0;
+        var startTime = DateTime.Now;
+        for (int a = 0; a < _numbers.Length; a++)
+        {
+            for (int b = a; b < _numbers.Length; b++)
+            {
+                var currentSum = 0;
+                for (int k = a; k <= b; k++)
+                {
+                    currentSum += _numbers[k];
+                }
+
+                bestSum = Max(bestSum, currentSum);
+            }
+        }
+
+        var totalTime = (DateTime.Now - startTime).TotalMicroseconds;
+        var recalculatedTotalTime = RecalculateTime(totalTime);
+        WriteLine(CreateResults("n^3", bestSum, recalculatedTotalTime));
+
+
+        bestSum = 0;
+        startTime = DateTime.Now;
+        for (int a = 0; a < _numbers.Length; a++)
+        {
+            var currentSum = 0;
+            for (int b = a; b < _numbers.Length; b++)
+            {
+                currentSum += _numbers[b];
+                bestSum = Max(bestSum, currentSum);
+            }
+        }
+
+        totalTime = (DateTime.Now - startTime).TotalMicroseconds;
+        recalculatedTotalTime = RecalculateTime(totalTime);
+        WriteLine(CreateResults("n^2", bestSum, recalculatedTotalTime));
+
+        bestSum = 0;
+        int sum = 0;
+        startTime = DateTime.Now;
+        for (int a = 0; a < _numbers.Length; a++)
+        {
+            sum = Max(_numbers[a], sum + _numbers[a]);
+            bestSum = Max(bestSum, sum);
+        }
+
+        totalTime = (DateTime.Now - startTime).TotalMicroseconds;
+        recalculatedTotalTime = RecalculateTime(totalTime);
+        WriteLine(CreateResults("n^1", bestSum, recalculatedTotalTime));
+    }
+
+
+    string RecalculateTime(double timeSpan)
+    {
+        var timeSpanUnit = "microseconds";
+        if (timeSpan > 1000)
+        {
+            timeSpan /= 1000;
+            timeSpanUnit = "milliseconds";
+        }
+
+        if (timeSpan > 1000)
+        {
+            timeSpan /= 1000;
+            timeSpanUnit = "seconds";
+        }
+
+        if (timeSpan > 60 && timeSpanUnit == "seconds")
+        {
+            timeSpan /= 60;
+            timeSpanUnit = "minutes";
+        }
+
+        return $"{timeSpan:N2} {timeSpanUnit}";
+    }
+
+    string CreateResults(string algorithmComplexity, int bestSum, string totalTimeWithUnit) =>
+        $"O({algorithmComplexity}) best sum='{bestSum}' took {totalTimeWithUnit} with {_numbers.Length} elements.";
+}
--- MaximumSubarraySum/Program.cs
+++ MaximumSubarraySum/Program.cs
@@ -1,87 +1,10 @@
-﻿using static System.Console;
-using static System.Math;
+﻿using MaximumSubarraySum;
 
-// var numbers = new[] { -1, 2, 4, -3, 5, 2, -5, 2 };
-const int exponent = 3;
-var numbers = Enumerable
-    .Range(0, (int)Pow(10, exponent))
-    .Select(_ => new Random().Next(-10, 11))
-    .ToArray();
+var benchmarkWith10Elements = new Calculation(1);
+benchmarkWith10Elements.Calculate();
 
-var bestSum = 0;
-var startTime = DateTime.Now;
-for (int a = 0; a < numbers.Length; a++)
-{
-    for (int b = a; b < numbers.Length; b++)
-    {
-        var currentSum = 0;
-        for (int k = a; k <= b; k++)
-        {
-            currentSum += numbers[k];
-        }
+var benchmarkWith100Elements = new Calculation(2);
+benchmarkWith100Elements.Calculate();
 
-        bestSum = Max(bestSum, currentSum);
-    }
-}
-
-var totalTime = (DateTime.Now - startTime).TotalMicroseconds;
-var recalculatedTotalTime = RecalculateTime(totalTime);
-WriteLine(CreateResults("n^3", bestSum, recalculatedTotalTime));
-
-
-bestSum = 0;
-startTime = DateTime.Now;
-for (int a = 0; a < numbers.Length; a++)
-{
-    var currentSum = 0;
-    for (int b = a; b < numbers.Length; b++)
-    {
-        currentSum += numbers[b];
-        bestSum = Max(bestSum, currentSum);
-    }
-}
-
-totalTime = (DateTime.Now - startTime).TotalMicroseconds;
-recalculatedTotalTime = RecalculateTime(totalTime);
-WriteLine(CreateResults("n^2", bestSum, recalculatedTotalTime));
-
-bestSum = 0;
-int sum = 0;
-startTime = DateTime.Now;
-for (int a = 0; a < numbers.Length; a++)
-{
-    sum = Max(numbers[a], sum + numbers[a]);
-    bestSum = Max(bestSum, sum);
-}
-
-totalTime = (DateTime.Now - startTime).TotalMicroseconds;
-recalculatedTotalTime = RecalculateTime(totalTime);
-WriteLine(CreateResults("n", bestSum, recalculatedTotalTime));
-
-
-string RecalculateTime(double timeSpan)
-{
-    var timeSpanUnit = "microseconds";
-    if (timeSpan > 1000)
-    {
-        timeSpan /= 1000;
-        timeSpanUnit = "milliseconds";
-    }
-
-    if (timeSpan > 1000)
-    {
-        timeSpan /= 1000;
-        timeSpanUnit = "seconds";
-    }
-
-    if (timeSpan > 60 && timeSpanUnit == "seconds")
-    {
-        timeSpan /= 60;
-        timeSpanUnit = "minutes";
-    }
-
-    return $"{timeSpan:N2} {timeSpanUnit}";
-}
-
-string CreateResults(string algorithmComplexity, int bestSum, string totalTimeWithUnit) =>
-    $"O({algorithmComplexity}) best sum='{bestSum}' took {totalTimeWithUnit}";
+var benchmarkWith1000Elements = new Calculation(3);
+benchmarkWith1000Elements.Calculate();
