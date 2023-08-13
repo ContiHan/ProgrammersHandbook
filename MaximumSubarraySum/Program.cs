181946b5487b82c41b7772ab1e1dc526d8fb3029
Daniel Hanák <daniel.hanak@outlook.cz>
2023-08-13 14:30:16 +0200

    O(n^3) complexity

--- MaximumSubarraySum/Program.cs
+++ MaximumSubarraySum/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+var numbers = new[] { -1, 2, 4, -3, 5, 2, -5, 2 };
+
+var bestSum = 0;
+
+for (int a = 0; a < numbers.Length; a++)
+{
+    for (int b = a; b < numbers.Length; b++)
+    {
+        var currentSum = 0;
+        for (int k = a; k <= b; k++)
+        {
+            currentSum += numbers[k];
+        }
+        bestSum = Math.Max(bestSum, currentSum);
+    }
+}
+
+WriteLine(bestSum);
