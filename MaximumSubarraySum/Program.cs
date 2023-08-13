99625e53604d4e27ffaa88151524adbd8ae69a89
Daniel Hanák <daniel.hanak@outlook.cz>
2023-08-13 21:03:41 +0200

    added 10000 elements to benchmark

--- MaximumSubarraySum/Program.cs
+++ MaximumSubarraySum/Program.cs
@@ -1,10 +1,17 @@
 ﻿using MaximumSubarraySum;
+using static System.Console;
 
 var benchmarkWith10Elements = new ArrayBenchmark(10);
 benchmarkWith10Elements.CalculateAllComplexities();
+WriteLine();
 
 var benchmarkWith100Elements = new ArrayBenchmark(100);
 benchmarkWith100Elements.CalculateAllComplexities();
+WriteLine();
 
 var benchmarkWith1000Elements = new ArrayBenchmark(1000);
 benchmarkWith1000Elements.CalculateAllComplexities();
+WriteLine();
+
+var benchmarkWith10000Elements = new ArrayBenchmark(10000);
+benchmarkWith10000Elements.CalculateAllComplexities();
